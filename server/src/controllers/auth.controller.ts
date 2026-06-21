79ef36aac190bfff0c5966c109e7ce8f0d936153
krishna sahu <krishna.sahu.work@gmail.com>
2026-06-21 16:45:41 +0530

    google auth controller add

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,41 +1,58 @@
 import userModel from "../models/user.model.js";
 import { googleAuthSchema } from "../schemas/auth.schema.js";
-import type { Response , Request } from "express";
+import type { Response, Request } from "express";
 import generateToken from "../utils/token.js";
+import { AUTH_COOKIE_NAME, AUTH_COOKIE_OPTIONS } from "../config/cookie.js";
 
-export async function googleAuth(req:Request , res:Response){
+export async function googleAuth(req: Request, res: Response) {
 
-const parsedData = googleAuthSchema.safeParse(req.body) ;
+    const parsedData = googleAuthSchema.safeParse(req.body);
 
-if(!parsedData.success){
-    return res.status(400).json({
-        success:false,
-        message:"Invalid Credentials",
-        error:parsedData.error.flatten()
-    }) ;
+    if (!parsedData.success) {
+        return res.status(400).json({
+            success: false,
+            message: "Invalid Credentials",
+            error: parsedData.error.flatten()
+        });
+    };
+
+    const { name, email, photoURL } = parsedData.data;
+
+    try {
+
+        let user = await userModel.findOne({ email });
+
+        const userData = {
+
+            name,
+            email,
+            ...(photoURL && { photoURL }),
+        }
+
+        if (!user) {
+
+            user = await userModel.create(userData);
+
+        }
+
+        let token = await generateToken(user._id);
+
+        res.cookie(AUTH_COOKIE_NAME, token, AUTH_COOKIE_OPTIONS);
+
+        return res.status(201).json({
+            success: true,
+            message: "User created Successfully",
+            data: {
+                user
+            }
+        });
+    } catch (error) {
+
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        });
+    };
+
 };
-
-const {name , email , photoURL}  = parsedData.data ;
-
-try{
-
-let user = await userModel.findOne({email}) ;
-
-const userData = {
-    
-    name , 
-    email , 
-    ...(photoURL &&{photoURL}),
-},
-
-if(!user){
-
-    user = await userModel.create(userData) ;
-
-}
-
-const token =await  generateToken(user._id) ; 
-
-}
-
-}
