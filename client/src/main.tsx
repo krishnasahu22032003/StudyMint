15240552aefec76a4fb5342dcbd478ec14fd7513
krishna sahu <krishna.sahu.work@gmail.com>
2026-06-22 00:45:05 +0530

    user slice client function add

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,12 +4,16 @@ import './index.css'
 import App from './App.tsx'
 import { ThemeProvider } from './context/ThemeContext.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import store from './redux/store.ts'
+import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <ThemeProvider>
+        <Provider store={store}>
         <App />
+        </Provider>
       </ThemeProvider>
     </BrowserRouter>
   </StrictMode>,
