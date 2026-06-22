dd52c3402d7397effc391b1478880d1a5e6d89ec
krishna sahu <krishna.sahu.work@gmail.com>
2026-06-23 00:54:32 +0530

    gemini service add

--- server/src/lib/ENV_SECRETS.ts
+++ server/src/lib/ENV_SECRETS.ts
@@ -4,7 +4,8 @@ const ENV_SECRETS = {
 
 BACKEND_PORT : process.env.PORT,
 DB_URL:process.env.MONGO_URL,
-JWT_SECRET:process.env.JWT_SECRET 
+JWT_SECRET:process.env.JWT_SECRET ,
+GEMINI_KEY:process.env.GEMINI_API_KEY
 
 };
 
---
+++ server/src/services/gemini.service.ts
@@ -0,0 +1,63 @@
+import ENV_SECRETS from "../lib/ENV_SECRETS.js";
+
+export const generateGeminiResponse = async (prompt: string) => {
+  const apiKey = ENV_SECRETS.GEMINI_KEY;
+
+  if (!apiKey) {
+    throw new Error("GEMINI_KEY is missing");
+  }
+
+  try {
+    const response = await fetch(
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${apiKey}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          contents: [
+            {
+              parts: [
+                {
+                  text: prompt,
+                },
+              ],
+            },
+          ],
+        }),
+      }
+    );
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      throw new Error(
+        `Gemini API Error ${response.status}: ${errorText}`
+      );
+    }
+
+    const data = await response.json();
+
+    const text =
+      data.candidates?.[0]?.content?.parts?.[0]?.text;
+
+    if (!text) {
+      throw new Error("No text returned from Gemini");
+    }
+
+    const cleanText = text
+      .replace(/```json/g, "")
+      .replace(/```/g, "")
+      .trim();
+
+    try {
+      return JSON.parse(cleanText);
+    } catch {
+      console.error("Invalid JSON:", cleanText);
+      throw new Error("Gemini returned invalid JSON");
+    }
+  } catch (error: any) {
+    console.error("Gemini Fetch Error:", error.message);
+    throw error;
+  }
+};
