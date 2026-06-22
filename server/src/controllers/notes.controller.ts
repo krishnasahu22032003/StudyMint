732eec6ad5ab94974cb7ce0315416c851d7d30cf
krishna sahu <krishna.sahu.work@gmail.com>
2026-06-23 04:22:04 +0530

    get single note controller add

--- server/src/controllers/notes.controller.ts
+++ server/src/controllers/notes.controller.ts
@@ -36,3 +36,47 @@ export const getUserNotes = async (
     });
   }
 };
+
+export const getSingleNotes = async (
+  req: Request,
+  res: Response
+) => {
+  if (!req.userId) {
+    return res.status(401).json({
+      success: false,
+      message: "Unauthorized",
+    });
+  }
+
+  try {
+    const noteId = req.params.id;
+
+    const note = await Notes.findOne({
+      _id: noteId,
+      user: req.userId,
+    })
+      .select(
+        "topic content createdAt classLevel examType revisionMode includeDiagram includeChart"
+      )
+      .lean();
+
+    if (!note) {
+      return res.status(404).json({
+        success: false,
+        message: "Note not found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      data: note,
+    });
+  } catch (error) {
+    console.error("Get Single Note Error:", error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Failed to fetch note",
+    });
+  }
+};
--- server/src/routes/NotesRoutes.ts
+++ server/src/routes/NotesRoutes.ts
@@ -1,9 +1,12 @@
 import express from "express" ;
 import authMiddleware from "../middlewares/AuthMiddleware.js";
 import { generateNotes } from "../controllers/generate.controller.js";
+import { getSingleNotes, getUserNotes } from "../controllers/notes.controller.js";
 
 const NotesRouter = express.Router() ;
 
 NotesRouter.post("/generate-notes" , authMiddleware , generateNotes );
+NotesRouter.get("/get-notes" , authMiddleware ,getUserNotes );
+NotesRouter.get("/:id" , authMiddleware ,getSingleNotes );
 
 export default NotesRouter ;
