9a921410e06861d790de10d98a1da14114e1bc33
krishna sahu <krishna.sahu.work@gmail.com>
2026-06-22 22:54:33 +0530

    app.tsx code update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,50 +1,69 @@
-import { useEffect } from 'react'
-import LandingPage from './pages/LandingPage'
-import { Routes, Route, Navigate } from 'react-router-dom'
-import getUserDetails from './lib/getuserdetails'
-import { useDispatch, useSelector } from 'react-redux'
-import type { RootState } from './redux/store'
-import DashboardPage from './pages/DashboardPage'
+import { useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { Toaster } from "sonner";
+import { useDispatch, useSelector } from "react-redux";
 
-type Props = {}
+import type { RootState } from "./redux/store";
+
+import LandingPage from "./pages/LandingPage";
+import DashboardPage from "./pages/DashboardPage";
+
+import getUserDetails from "./lib/getuserdetails";
+import AppLayout from "./layouts/AppLayout";
+
+type Props = {};
 
 const App = (props: Props) => {
-
   const dispatch = useDispatch();
 
   useEffect(() => {
-
     getUserDetails(dispatch);
-
   }, [dispatch]);
 
-  const { userData } = useSelector((state: RootState) => state.user)
+  const { userData } = useSelector(
+    (state: RootState) => state.user
+  );
 
   return (
     <>
-    <Toaster richColors position='top-center'/>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            userData
-              ? <Navigate to="/dashboard" replace />
-              : <LandingPage />
-          }
-        />
+      <Toaster
+        richColors
+        position="top-center"
+      />
 
-        <Route
-          path="/dashboard"
-          element={
-            userData
-              ? <DashboardPage />
-              : <Navigate to="/" replace />
-          }
-        />
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route
+            path="/"
+            element={
+              userData ? (
+                <Navigate
+                  to="/dashboard"
+                  replace
+                />
+              ) : (
+                <LandingPage />
+              )
+            }
+          />
+
+          <Route
+            path="/dashboard"
+            element={
+              userData ? (
+                <DashboardPage />
+              ) : (
+                <Navigate
+                  to="/"
+                  replace
+                />
+              )
+            }
+          />
+        </Route>
       </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
