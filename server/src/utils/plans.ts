807c2395ebc59854eb93ffc671fa8a9e4d3b72e9
krishna sahu <krishna.sahu.work@gmail.com>
2026-06-24 00:20:09 +0530

    plans added

---
+++ server/src/utils/plans.ts
@@ -0,0 +1,16 @@
+export const CREDIT_PLANS = {
+  starter: {
+    amount: 99,
+    credits: 99,
+  },
+
+  pro: {
+    amount: 299,
+    credits: 299,
+  },
+
+  premium: {
+    amount: 999,
+    credits: 999,
+  },
+};
