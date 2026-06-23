e61a8b9fc66aca037cf0263aefa80ee6ca577e1a
krishna sahu <krishna.sahu.work@gmail.com>
2026-06-24 00:19:42 +0530

    update code

--- server/src/lib/ENV_SECRETS.ts
+++ server/src/lib/ENV_SECRETS.ts
@@ -5,7 +5,9 @@ const ENV_SECRETS = {
 BACKEND_PORT : process.env.PORT,
 DB_URL:process.env.MONGO_URL,
 JWT_SECRET:process.env.JWT_SECRET ,
-GEMINI_KEY:process.env.GEMINI_API_KEY
+GEMINI_KEY:process.env.GEMINI_API_KEY, 
+key_id: process.env.RAZORPAY_KEY_ID,
+key_secret: process.env.RAZORPAY_KEY_SECRET,
 
 };
 
